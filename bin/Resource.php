179b9007c2a3f9808f27f574ba8c0e35e5f68321
Pumahawk <Lorenzoga94@gmail.com>
2017-01-31 19:29:49 +0100

    Inizializzata la classe Resource

---
+++ bin/Resource.php
@@ -0,0 +1,12 @@
+<?php
+namespace Bin;
+
+class Resource{
+	public static function outPutFile($path){
+		//function da sviluppare
+	}
+	
+	public static function saveFile($path){
+		//function da sviluppare
+	}
+}
