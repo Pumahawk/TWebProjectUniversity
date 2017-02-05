2f4cf58fa3efaf5e239109296f854a2d1fa35b4b
Pumahawk <Lorenzoga94@gmail.com>
2017-02-05 11:26:45 +0100

    Risolto bug
    Bug che non permetteva di vedere la descrizione dei prodotti ai non
    amministratori

--- app/Router.php
+++ app/Router.php
@@ -10,7 +10,7 @@
 			Router::get("helloword", 'App\Router::helloword');
 			Router::get("testview", 'App\View\Test::test');
 			
-			Router::get("getInfoProd", "App\Controller\ProductController::getFromId") -> middleware("isAdmin");
+			Router::get("getInfoProd", "App\Controller\ProductController::getFromId");
 			Router::get("/", "App\Controller\ProductController::homePage");
 			Router::get("notAdmin", "App\View\User::notAdminPage");
 			Router::get("addProductToCart", "App\Controller\ProductController::addToCart");
@@ -18,7 +18,7 @@
 			Router::get("openImg", "App\Controller\ProductController::openImg");
 			Router::get("registrationPage", "App\View\SitePage::registrationPage");
 			Router::get("registrationJSON", "App\Controller\UserController::registrationJSON");
-			Router::get("modificaUtenteJSON", "App\Controller\UserController::modificaUtenteJSON");
+			Router::get("modificaUtenteJSON", "App\Controller\UserController::modificaUtenteJSON") -> middleware("logged");
 			//Router::get("contact", "App\View\SitePage::contactPage");
 			Router::get("manageProducts", "App\Controller\ProductController::manageProductsPage") -> middleware("isAdmin");
 			Router::get("optionProduct", "App\View\SitePage::optionProductPage") -> middleware("isAdmin");
