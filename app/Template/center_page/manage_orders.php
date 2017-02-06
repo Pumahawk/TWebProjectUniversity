695727cd6d6f77e4f33fa03eb236e557b4e6d8fc
Pumahawk <Lorenzoga94@gmail.com>
2017-02-06 22:19:00 +0100

    Aggiunte icone ai pulsanti

--- app/Template/center_page/manage_orders.php
+++ app/Template/center_page/manage_orders.php
@@ -2,7 +2,7 @@
   	<tr><td>ID</td><td>Data</td><td>Stato</td><td>Azioni</td>
   <?php if($this->productsList != null)foreach ($this->productsList as $ordine):?>
   	<tr><td><?=$ordine["id"]?></td><td><?=$ordine["data"]?></td><td><?=($ordine["stato"] == "in_consegna") ? "Prodotto in fase di elaborazione" : "Prodotto consegnato"?></td><td>
-  	<button class = "btn btn-default btn-visualizza-ordine" data-toggle="modal" data-id="<?= $ordine["id"]?>" data-target="#orderView">Apri</button>
+  	<button class = "btn btn-default btn-visualizza-ordine" data-toggle="modal" data-id="<?= $ordine["id"]?>" data-target="#orderView"><span class = "glyphicon glyphicon-eye-open"></span></button>
   	<?php if($_SESSION["utente"]["tipo"] == "admin" && $ordine["stato"] == "in_consegna"):?> <button data-id = "<?= $ordine["id"]?>" class = "set-to-cons btn btn-default">Consegna</button>
   	<?php endif;?></td>
   <?php endforeach;?>
--- app/Template/center_page/manage_product.php
+++ app/Template/center_page/manage_product.php
@@ -1,10 +1,10 @@
 
-<button class = "btn btn-default" data-toggle="modal" data-target="#newProduct">Aggiungi</button>
+<button class = "btn btn-default" data-toggle="modal" data-target="#newProduct"><span class = "glyphicon glyphicon-plus"></span> Aggiungi</button>
 <hr />
 <table class="table table-striped">
   	<tr><td>ID</td><td>Titolo</td><td>Azioni</td>
   <?php if($this->productsList != null)foreach ($this->productsList as $ordine):?>
-  	<tr><td><?=$ordine["id"]?></td><td><?=$ordine["titolo"]?></td><td><button id = "prodotto" class = "btn-visualizza-prodotto btn btn-default" data-toggle="modal" data-target="#productView" data-id = "<?=$ordine["id"]?>">Visualizza</button></td>
+  	<tr><td><?=$ordine["id"]?></td><td><?=$ordine["titolo"]?></td><td><button id = "prodotto" class = "btn-visualizza-prodotto btn btn-default" data-toggle="modal" data-target="#productView" data-id = "<?=$ordine["id"]?>"><span class = "glyphicon glyphicon-eye-open"></span></button></td>
   <?php endforeach;?>
 </table>
 
